25075fdacd09305820f8b8093bc49c05c2802f82
Will Tyler <will.tyler11@gmail.com>
2019-01-14 23:42:11 +0000

    Basic setup

--- Flix/AppDelegate.swift
+++ Flix/AppDelegate.swift
@@ -8,14 +8,20 @@
 
 import UIKit
 
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window: UIWindow?
 
-
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		// Override point for customization after application launch.
+		window = UIWindow(frame: UIScreen.main.bounds)
+
+		let viewController = ViewController()
+
+		window!.rootViewController = viewController
+		window!.makeKeyAndVisible()
+
 		return true
 	}
 
@@ -41,6 +47,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 	}
 
-
 }
-
